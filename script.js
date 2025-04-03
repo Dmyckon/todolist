dac55087e2612ff6e07b261289b2c28137b98975
Dmyckon <dmyckon@gmail.com>
2025-04-03 15:08:39 +0100

    add functinal and styling scripts

--- script.js
+++ script.js
@@ -20,10 +20,29 @@ function darkTheme() {
     document.getElementById("back2").style.display = "none"
 }
 
+
+
 function checkMark() {
-    document.getElementById("storage1").innerHTML = 
+    document.getElementById("store1").innerHTML = 
     document.getElementById("enter1").value
-    document.getElementById("storage2").innerHTML = 
+    document.getElementById("store2").innerHTML = 
     document.getElementById("enter2").value
-    
+
+    document.getElementById("storage1").style.display = "flex"
+    document.getElementById("storage2").style.display = "flex"
+    document.getElementById("enter1").value = ""
+    document.getElementById("enter2").value = ""
+
+}
+
+function crossCheck() {
+    document.getElementById("store1").style.textDecoration = "line-through"
+    document.getElementById("store2").style.textDecoration = "line-through"
+}
+
+function cancelCheck() {
+    document.getElementById("store1").style.textDecoration = "none"
+    document.getElementById("store2").style.textDecoration = "none"
+    document.getElementById("store1").innerHTML = ""
+    document.getElementById("store2").innerHTML = ""
 }
